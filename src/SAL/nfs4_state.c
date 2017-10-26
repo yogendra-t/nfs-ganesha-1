ffilz/nfs-ganesha

Use separate entry for cache_open_owners list
Previously, the so_state_list filed of the owner was used both as a list
head and as a list entry.  Separate this out.

Change-Id: I301b1ff867916cca75939f9531294839d1238dc4
Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

src/SAL/nfs4_state.c
@@ -403,7 +403,8 @@ void state_del_locked(state_t *state)
 		 * owner, we will want to retain a refcount and let the
 		 * reaper thread clean up with owner. */
 		owner_retain = owner->so_type == STATE_OPEN_OWNER_NFSV4 &&
-		    glist_empty(&nfs4_owner->so_state_list);
+		    glist_empty(&nfs4_owner->so_state_list) &&
+		    glist_empty(&nfs4_owner->so_cache_entry);
 
 		PTHREAD_MUTEX_unlock(&state->state_mutex);
 
@@ -417,7 +418,7 @@ void state_del_locked(state_t *state)
 					    nfs_param.nfsv4_param.lease_lifetime
 						+ time(NULL));
 			glist_add_tail(&cached_open_owners,
-				       &nfs4_owner->so_state_list);
+				       &nfs4_owner->so_cache_entry);
 
 			if (isFullDebug(COMPONENT_STATE)) {
 				char str[LOG_BUFF_LEN];

yogendra-t/nfs-ganesha-1

(cherry picked from commit d8cdc8b5d2a4ae77ca2e924621a3ff7b430d1af3)

src/SAL/nfs4_state.c
@@ -386,7 +386,8 @@ void state_del_locked(state_t *state)
 		 * owner, we will want to retain a refcount and let the
 		 * reaper thread clean up with owner. */
 		owner_retain = owner->so_type == STATE_OPEN_OWNER_NFSV4 &&
-		    glist_empty(&nfs4_owner->so_state_list);
+		    glist_empty(&nfs4_owner->so_state_list) &&
+		    glist_null(&nfs4_owner->so_cache_entry);
 
 		PTHREAD_MUTEX_unlock(&state->state_mutex);
 
@@ -400,7 +401,7 @@ void state_del_locked(state_t *state)
 					    nfs_param.nfsv4_param.lease_lifetime
 						+ time(NULL));
 			glist_add_tail(&cached_open_owners,
-				       &nfs4_owner->so_state_list);
+				       &nfs4_owner->so_cache_entry);
 
 			if (isFullDebug(COMPONENT_STATE)) {
 				char str[LOG_BUFF_LEN];