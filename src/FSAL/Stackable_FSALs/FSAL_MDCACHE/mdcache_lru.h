81066ed66d8f28b0d58dc449c716fc00a799fc42
Kaleb S. KEITHLEY <kkeithle@redhat.com>
2018-08-02 09:41:05 -0400

    Subject: [PATCH 2/6] MDCACHE: For dirent chunking, prevent prev_chunk from being reaped
    Change-Id: I16d021e103a67315c4c3c60c68903079030ef221
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>
    (cherry picked from commit f1209afadb772ebe01bb3e0bf206fd7d77bba99f)

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_lru.h
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_lru.h
@@ -200,7 +200,8 @@ static inline bool mdcache_lru_caching_fds(void)
 }
 
 void lru_remove_chunk(struct dir_chunk *chunk);
-struct dir_chunk *mdcache_get_chunk(mdcache_entry_t *parent);
+struct dir_chunk *mdcache_get_chunk(mdcache_entry_t *parent,
+				    struct dir_chunk *prev_chunk);
 void lru_bump_chunk(struct dir_chunk *chunk);
 
 #endif				/* MDCACHE_LRU_H */
