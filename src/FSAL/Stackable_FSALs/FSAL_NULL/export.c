madhuthorat/nfs-ganesha

Add 'expire_time_parent' parameter and refill parent handle if expired
If the underlying file system doesn't give sufficient
information to notify about parent change for a DIRECTORY then
the parent handle won't get updated for an entry. This may cause
problem when parent handle is used.
This change intends to help with this problem by providing a new
parameter 'expire_time_parent' to let a FSAL set expiration time
for parent handle. The default value is 'expire_time_parent=-1',
which means ganesha won't expire parent handle. If a FSAL sets
'expire_time_parent' to any other value then whenever we try to
get parent handle, we check if the current parent handle has expired
by checking the current time with 'expire_time_parent'. In case it
has expired then if required a lookup is made for the parent and
the parent handle is updated.

Change-Id: I76a022139c08052817f1f29ddcf77ce4d7d51b3f
Signed-off-by: Madhu Thorat <madhu.punjabi@in.ibm.com>

src/FSAL/Stackable_FSALs/FSAL_NULL/export.c
@@ -263,6 +263,20 @@ static uint32_t fs_xattr_access_rights(struct fsal_export *exp_hdl)
 	return result;
 }
 
+static int32_t fs_expiretimeparent(struct fsal_export *exp_hdl)
+{
+	struct nullfs_fsal_export *exp =
+		container_of(exp_hdl, struct nullfs_fsal_export, export);
+
+	op_ctx->fsal_export = exp->export.sub_export;
+	uint32_t result =
+		exp->export.sub_export->exp_ops.fs_expiretimeparent(
+				exp->export.sub_export);
+	op_ctx->fsal_export = &exp->export;
+
+	return result;
+}
+
 /* get_quota
  * return quotas for this export.
  * path could cross a lower mount boundary which could
@@ -421,6 +435,7 @@ void nullfs_export_ops_init(struct export_ops *ops)
 	ops->fs_supported_attrs = fs_supported_attrs;
 	ops->fs_umask = fs_umask;
 	ops->fs_xattr_access_rights = fs_xattr_access_rights;
+	ops->fs_expiretimeparent = fs_expiretimeparent;
 	ops->get_quota = get_quota;
 	ops->set_quota = set_quota;
 	ops->alloc_state = nullfs_alloc_state;

yogendra-t/nfs-ganesha-1

If the underlying file system doesn't give sufficient information
in an upcall to notify about parent change for a DIRECTORY then
Conflicts:
	src/FSAL/FSAL_CEPH/main.c
	src/FSAL/FSAL_GLUSTER/main.c
	src/FSAL/FSAL_GPFS/main.c
	src/FSAL/FSAL_MEM/mem_main.c
	src/FSAL/FSAL_PROXY/main.c
	src/FSAL/FSAL_PSEUDO/main.c
	src/FSAL/FSAL_RGW/main.c
	src/FSAL/FSAL_VFS/panfs/main.c
	src/FSAL/FSAL_VFS/vfs/main.c
	src/FSAL/FSAL_VFS/xfs/main.c
	src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_export.c
	src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
	src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_int.h
	src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_main.c
	src/FSAL/Stackable_FSALs/FSAL_NULL/export.c
	src/FSAL/Stackable_FSALs/FSAL_NULL/main.c
	src/FSAL/commonlib.c
	src/FSAL/fsal_config.c
	src/include/FSAL/fsal_config.h
(cherry picked from commit 1826df94895f99804a5adaa20c1ed062a856daa9)

src/FSAL/Stackable_FSALs/FSAL_NULL/export.c
@@ -263,6 +263,20 @@ static uint32_t fs_xattr_access_rights(struct fsal_export *exp_hdl)
 	return result;
 }
 
+static int32_t fs_expiretimeparent(struct fsal_export *exp_hdl)
+{
+	struct nullfs_fsal_export *exp =
+		container_of(exp_hdl, struct nullfs_fsal_export, export);
+
+	op_ctx->fsal_export = exp->export.sub_export;
+	int32_t result =
+		exp->export.sub_export->exp_ops.fs_expiretimeparent(
+				exp->export.sub_export);
+	op_ctx->fsal_export = &exp->export;
+
+	return result;
+}
+
 /* get_quota
  * return quotas for this export.
  * path could cross a lower mount boundary which could
@@ -421,6 +435,7 @@ void nullfs_export_ops_init(struct export_ops *ops)
 	ops->fs_supported_attrs = fs_supported_attrs;
 	ops->fs_umask = fs_umask;
 	ops->fs_xattr_access_rights = fs_xattr_access_rights;
+	ops->fs_expiretimeparent = fs_expiretimeparent;
 	ops->get_quota = get_quota;
 	ops->set_quota = set_quota;
 	ops->alloc_state = nullfs_alloc_state;