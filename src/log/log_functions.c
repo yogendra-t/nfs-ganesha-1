84e969b0d3befd3577fdbafb7ab14bcfb79dbaa0
Madhu Thorat <madhu.punjabi@in.ibm.com>
2019-05-07 16:13:45 -0400

    Change rpc_warnx log level check from NIV_FATAL to NIV_DEBUG
    Changed rpc_warnx component_log_level check from NIV_FATAL to NIV_DEBUG
    This is to avoid seeing EVENT traces by default for ntirpc component.
    
    Reported-by: Ravi K Komanduri <rkomandu@in.ibm.com>
    Signed-off-by: Madhu Thorat <madhu.punjabi@in.ibm.com>

--- src/log/log_functions.c
+++ src/log/log_functions.c
@@ -1722,7 +1722,7 @@ void rpc_warnx(char *fmt, ...)
 {
 	va_list ap;
 
-	if (component_log_level[COMPONENT_TIRPC] <= NIV_FATAL)
+	if (component_log_level[COMPONENT_TIRPC] < NIV_DEBUG)
 		return;
 
 	va_start(ap, fmt);
