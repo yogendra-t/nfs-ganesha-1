madhuthorat/nfs-ganesha

Don't call mdcache_locate_host in mdc_lookup if mdc_get_parent fails
Currently in mdc_lookup() we pass 'tmpfh' variable to mdc_get_parent().
But if lookup() fails in mdc_get_parent() and if
entry->fsobj.fsdir.parent.len==0 then the 'tmpfh' passed by mdc_lookup()
doesn't get filled. After this the uninitalized 'tmpfh' in mdc_lookup()
gets passed to mdcache_locate_host() and its usage leads to a crash.
Fixed this by returning 'status' from the mdc_get_parent(), in case of
error status we don't call mdcache_locate_host() now.

Change-Id: I4c80df269e00960146135e0c67bd1964b9f9a3e1
Signed-off-by: Madhu Thorat <madhu.punjabi@in.ibm.com>

src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_int.h
@@ -504,7 +504,7 @@ fsal_status_t mdcache_readdir_chunked(mdcache_entry_t *directory,
 				      attrmask_t attrmask,
 				      bool *eod_met);
 
-void mdc_get_parent(struct mdcache_fsal_export *exp,
+fsal_status_t mdc_get_parent(struct mdcache_fsal_export *exp,
 		    mdcache_entry_t *entry,
 		    struct gsh_buffdesc *parent_out);
 

yogendra-t/nfs-ganesha-1

Conflicts:
	src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
	src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_int.h

src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_int.h
@@ -504,7 +504,7 @@ fsal_status_t mdcache_readdir_chunked(mdcache_entry_t *directory,
 				      attrmask_t attrmask,
 				      bool *eod_met);
 
-void mdc_get_parent(struct mdcache_fsal_export *export,
+fsal_status_t mdc_get_parent(struct mdcache_fsal_export *export,
 		    mdcache_entry_t *entry,
 		    struct gsh_buffdesc *parent_out);
 