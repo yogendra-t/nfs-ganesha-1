madhuthorat/nfs-ganesha

Release file system after it is unclaimed
Currently in GPFS after last export of a file system
is removed, the file system is unclaimed but not removed.
If a file system is deleted and a new file system is
added which has the same dev major, minor numbers as the
previous file system then it is skipped as duplicate file
system. Made changes by calling "release_posix_file_system"
after the file system is unclaimed.

Change-Id: I71c8a7e73398ab79ad97e7a11ce09baee676519c
Signed-off-by: Madhu Thorat <madhu.punjabi@in.ibm.com>

src/include/FSAL/fsal_commonlib.h
@@ -94,6 +94,8 @@ int resolve_posix_filesystem(const char *path,
 			     unclaim_filesystem_cb unclaim,
 			     struct fsal_filesystem **root_fs);
 
+void release_posix_file_system_locked(struct fsal_filesystem *fs);
+
 void release_posix_file_systems(void);
 
 int re_index_fs_fsid(struct fsal_filesystem *fs,

yogendra-t/nfs-ganesha-1

(cherry picked from commit c530d44d8b6e9c4e6aa4a49f00008e81d1330d09)

src/include/FSAL/fsal_commonlib.h
@@ -122,6 +122,8 @@ int resolve_posix_filesystem(const char *path,
 			     unclaim_filesystem_cb unclaim,
 			     struct fsal_filesystem **root_fs);
 
+void release_posix_file_system(struct fsal_filesystem *fs);
+
 void release_posix_file_systems(void);
 
 void release_posix_file_system(struct fsal_filesystem *fs);