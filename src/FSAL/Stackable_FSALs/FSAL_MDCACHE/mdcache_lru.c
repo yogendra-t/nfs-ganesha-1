madhuthorat/nfs-ganesha

Added missing tracepoint for lru_reap_chunk_impl
After incrementing/decrementing an entry's 'lru.refcnt' we
should try to have a tracepoint for it. As it was missing in
lru_reap_chunk_impl() added the same after incrementing an
entry's lru.refcnt.

Signed-off-by: Madhu Thorat <madhu.punjabi@in.ibm.com>

src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_lru.c
@@ -808,7 +808,18 @@ lru_reap_chunk_impl(enum lru_q_id qid, mdcache_entry_t *parent)
 				/* We need an LRU ref on parent entry to protect
 				 * it while we do work on it's chunk.
 				 */
-				(void) atomic_inc_int32_t(&entry->lru.refcnt);
+#ifdef USE_LTTNG
+				uint32_t refcnt =
+#endif
+					atomic_inc_int32_t(&entry->lru.refcnt);
+
+
+#ifdef USE_LTTNG
+				tracepoint(mdcache, mdc_lru_ref,
+					   __func__, __LINE__,
+					   &entry->obj_handle, entry->sub_handle,
+					   refcnt);
+#endif
 			}
 
 			/* Dequeue the chunk so it won't show up anymore */

yogendra-t/nfs-ganesha-1

(cherry picked from commit e83254c0e1b596409c5abb78c0b364531211a259)

src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_lru.c
@@ -808,7 +808,19 @@ lru_reap_chunk_impl(enum lru_q_id qid, mdcache_entry_t *parent)
 				/* We need an LRU ref on parent entry to protect
 				 * it while we do work on it's chunk.
 				 */
+#ifdef USE_LTTNG
+				int32_t refcnt =
+					atomic_inc_int32_t(&entry->lru.refcnt);
+#else
 				(void) atomic_inc_int32_t(&entry->lru.refcnt);
+#endif
+
+#ifdef USE_LTTNG
+				tracepoint(mdcache, mdc_lru_ref,
+					   __func__, __LINE__,
+					   &entry->obj_handle, entry->sub_handle,
+					   refcnt);
+#endif
 			}
 
 			/* Dequeue the chunk so it won't show up anymore */