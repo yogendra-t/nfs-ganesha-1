9a7bb47d57dc16a6505c0530c66b44a076c6d4bf
Malahal Naineni <malahal@us.ibm.com>
2019-11-12 21:13:06 +0530

    Fix memory leak in nfs3_readdirplus
    If there are no entries left and return 0 entries, we don't free
    tracker.entries memory in nfs3_readdirplus(). Make it similar to
    nfs3_readdir() so that we free this memory.
    
    Change-Id: I25e12dbff4cf1521d66f770c5e73bd137066f3b2
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>
    (cherry picked from commit 9c81f0239b89ce042cfc9b1208b6c28cb10ae161)

--- src/Protocols/NFS/nfs3_readdirplus.c
+++ src/Protocols/NFS/nfs3_readdirplus.c
@@ -365,9 +365,14 @@ int nfs3_readdirplus(nfs_arg_t *arg, struct svc_req *req, nfs_res_t *res)
 	if (dir_entry)
 		cache_inode_put(dir_entry);
 
-	if (((res->res_readdir3.status != NFS3_OK) || (rc != NFS_REQ_OK))
-	    && (tracker.entries != NULL))
+	/* Deallocate memory in the event of an error */
+	if (((res->res_readdir3.status != NFS3_OK) || (rc != NFS_REQ_OK) ||
+	    ((num_entries == 0) && (begin_cookie > 1))) &&
+	    (tracker.entries != NULL)) {
 		free_entryplus3s(tracker.entries);
+		res->res_readdirplus3.READDIRPLUS3res_u.resok.reply.entries =
+		    NULL;
+	}
 
 	return rc;
 }				/* nfs3_readdirplus */
