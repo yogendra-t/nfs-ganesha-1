sachinpunadikar/nfs-ganesha

Do not collect latency stats for client
Statistics counting per client/export, currently involves,
operation and latency statistics for read, write commands.
While collecting and extracting stats per client, latency value has
no meaning. Actually it is not the latency from client perspective,
rather it is server perspective.
The per client stats would be helpful in getting insights of the
workload exerted by the client. For this information on number of
operations and kind of operations is helpful.
Not collecting latency information while gathering stats for a client.

Change-Id: I547b613fc94599d2854506283ff85febf30a5d7b
Signed-off-by: Sachin Punadikar <psachin@in.ibm.com>

src/support/server_stats.c
@@ -692,6 +692,28 @@ static void record_op(struct proto_op *op, nsecs_elapsed_t request_time,
 	record_latency(op, request_time, dup);
 }
 
+/**
+ * @brief count the protocol operation only
+ *
+ * Use atomic ops to avoid locks. We don't lock for the max
+ * and min because if there is a collision, over the long haul,
+ * the error is near zero...
+ *
+ * @param op           [IN] pointer to specific protocol struct
+ * @param success      [IN] protocol error code == OK
+ * @param dup          [IN] true if op was detected duplicate
+ */
+
+static void record_op_only(struct proto_op *op, bool success, bool dup)
+{
+	/* count the op */
+	(void)atomic_inc_uint64_t(&op->total);
+	/* also count it as an error if protocol not happy */
+	if (!success)
+		(void)atomic_inc_uint64_t(&op->errors);
+	if (unlikely(dup))
+		(void)atomic_inc_uint64_t(&op->dups);
+}
 
 #ifdef USE_DBUS
 /**
@@ -904,7 +926,7 @@ static void record_layout(struct nfsv41_stats *sp, int proto_op, int status)
 static void record_nfsv4_op(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
 			    int proto_op, int minorversion,
 			    nsecs_elapsed_t request_time,
-			    int status)
+			    int status, bool is_export)
 {
 	if (minorversion == 0) {
 		struct nfsv40_stats *sp = get_v40(gsh_st, lock);
@@ -912,15 +934,27 @@ static void record_nfsv4_op(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
 		/* record stuff */
 		switch (nfsv40_optype[proto_op]) {
 		case READ_OP:
-			record_op(&sp->read.cmd, request_time,
+			if (is_export)
+				record_op(&sp->read.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->read.cmd,
 					status == NFS4_OK, false);
 			break;
 		case WRITE_OP:
-			record_op(&sp->write.cmd, request_time,
+			if (is_export)
+				record_op(&sp->write.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->write.cmd,
 					status == NFS4_OK, false);
 			break;
 		default:
-			record_op(&sp->compounds, request_time,
+			if (is_export)
+				record_op(&sp->compounds, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->compounds,
 					status == NFS4_OK, false);
 		}
 	} else if (minorversion == 1) {
@@ -929,19 +963,31 @@ static void record_nfsv4_op(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
 		/* record stuff */
 		switch (nfsv41_optype[proto_op]) {
 		case READ_OP:
-			record_op(&sp->read.cmd, request_time,
+			if (is_export)
+				record_op(&sp->read.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->read.cmd,
 					status == NFS4_OK, false);
 			break;
 		case WRITE_OP:
-			record_op(&sp->write.cmd, request_time,
+			if (is_export)
+				record_op(&sp->write.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->write.cmd,
 					status == NFS4_OK, false);
 			break;
 		case LAYOUT_OP:
 			record_layout(sp, proto_op, status);
 			break;
 		default:
-			record_op(&sp->compounds, request_time,
-				  status == NFS4_OK, false);
+			if (is_export)
+				record_op(&sp->compounds, request_time,
+					  status == NFS4_OK, false);
+			else
+				record_op_only(&sp->compounds,
+					  status == NFS4_OK, false);
 		}
 	} else if (minorversion == 2) {
 		struct nfsv41_stats *sp = get_v42(gsh_st, lock);
@@ -949,18 +995,30 @@ static void record_nfsv4_op(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
 		/* record stuff */
 		switch (nfsv42_optype[proto_op]) {
 		case READ_OP:
-			record_op(&sp->read.cmd, request_time,
+			if (is_export)
+				record_op(&sp->read.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->read.cmd,
 					status == NFS4_OK, false);
 			break;
 		case WRITE_OP:
-			record_op(&sp->write.cmd, request_time,
+			if (is_export)
+				record_op(&sp->write.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->write.cmd,
 					status == NFS4_OK, false);
 			break;
 		case LAYOUT_OP:
 			record_layout(sp, proto_op, status);
 			break;
 		default:
-			record_op(&sp->compounds, request_time,
+			if (is_export)
+				record_op(&sp->compounds, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->compounds,
 					status == NFS4_OK, false);
 		}
 	}
@@ -998,6 +1056,69 @@ static void record_compound(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
 	}
 
 }
+
+/**
+ * @brief Record request statistics (V3 era protos only)
+ *
+ * Decode the protocol and find the proto specific stats struct.
+ * Once we found the stats block, do the update(s).
+ *
+ * @param gsh_st       [IN] stats struct from client or export
+ * @param lock         [IN] lock on client|export for malloc
+ * @param reqdata      [IN] info about the proto request
+ * @param success      [IN] the op returned OK (or error)
+ * @param dup          [IN] detected this was a dup request
+ */
+
+static void record_clnt_stats(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
+			 nfs_request_t *reqdata, bool success, bool dup)
+{
+	struct svc_req *req = &reqdata->svc;
+	uint32_t proto_op = req->rq_msg.cb_proc;
+	uint32_t program_op = req->rq_msg.cb_prog;
+
+	if (program_op == NFS_program[P_NFS]) {
+		if (proto_op == 0)
+			return;	/* we don't count NULL ops */
+		if (req->rq_msg.cb_vers == NFS_V3) {
+			struct nfsv3_stats *sp = get_v3(gsh_st, lock);
+
+			/* record stuff */
+			switch (nfsv3_optype[proto_op]) {
+			case READ_OP:
+				record_op_only(&sp->read.cmd, success, dup);
+				break;
+			case WRITE_OP:
+				record_op_only(&sp->write.cmd, success, dup);
+				break;
+			default:
+				record_op_only(&sp->cmds, success, dup);
+			}
+		} else {
+			/* We don't do V4 here and V2 is toast */
+			return;
+		}
+	} else if (program_op == NFS_program[P_MNT]) {
+		struct mnt_stats *sp = get_mnt(gsh_st, lock);
+
+		if (req->rq_msg.cb_vers == MOUNT_V1)
+			record_op_only(&sp->v1_ops, success, dup);
+		else
+			record_op_only(&sp->v3_ops, success, dup);
+	} else if (program_op == NFS_program[P_NLM]) {
+		struct nlmv4_stats *sp = get_nlm4(gsh_st, lock);
+
+		record_op_only(&sp->ops, success, dup);
+	} else if (program_op == NFS_program[P_RQUOTA]) {
+		struct rquota_stats *sp = get_rquota(gsh_st, lock);
+
+		if (req->rq_msg.cb_vers == RQUOTAVERS)
+			record_op_only(&sp->ops, success, dup);
+		else
+			record_op_only(&sp->ext_ops, success, dup);
+	}
+}
+
 
 /**
  * @brief Record request statistics (V3 era protos only)
@@ -1213,9 +1334,8 @@ void server_stats_nfs_done(nfs_request_t *reqdata, int rc, bool dup)
 		struct server_stats *server_st;
 
 		server_st = container_of(client, struct server_stats, client);
-		record_stats(&server_st->st, &client->lock, reqdata,
-			     stop_time - op_ctx->start_time,
-			     rc == NFS_REQ_OK, dup, true);
+		record_clnt_stats(&server_st->st, &client->lock, reqdata,
+			     rc == NFS_REQ_OK, dup);
 		(void)atomic_store_uint64_t(&client->last_update, stop_time);
 	}
 	if (!dup && op_ctx->ctx_export != NULL) {
@@ -1226,7 +1346,7 @@ void server_stats_nfs_done(nfs_request_t *reqdata, int rc, bool dup)
 			    export);
 		record_stats(&exp_st->st, &op_ctx->ctx_export->lock, reqdata,
 			     stop_time - op_ctx->start_time,
-			     rc == NFS_REQ_OK, dup, false);
+			     rc == NFS_REQ_OK, dup, true);
 		(void)atomic_store_uint64_t(&op_ctx->ctx_export->last_update,
 					    stop_time);
 	}
@@ -1266,7 +1386,7 @@ void server_stats_nfsv4_op_done(int proto_op,
 		server_st = container_of(client, struct server_stats, client);
 		record_nfsv4_op(&server_st->st, &client->lock, proto_op,
 				op_ctx->nfs_minorvers, stop_time - start_time,
-				status);
+				status, false);
 		(void)atomic_store_uint64_t(&client->last_update, stop_time);
 	}
 
@@ -1288,7 +1408,7 @@ void server_stats_nfsv4_op_done(int proto_op,
 			    export);
 		record_nfsv4_op(&exp_st->st, &op_ctx->ctx_export->lock,
 				proto_op, op_ctx->nfs_minorvers,
-				stop_time - start_time, status);
+				stop_time - start_time, status, true);
 		(void)atomic_store_uint64_t(&op_ctx->ctx_export->last_update,
 					    stop_time);
 	}

yogendra-t/nfs-ganesha-1

Change-Id: Ide7db989bc02f5bb77169b76dcb3792a1d144c10

src/support/server_stats.c
@@ -698,6 +698,28 @@ static void record_op(struct proto_op *op, nsecs_elapsed_t request_time,
 	record_latency(op, request_time, dup);
 }
 
+/**
+ * @brief count the protocol operation only
+ *
+ * Use atomic ops to avoid locks. We don't lock for the max
+ * and min because if there is a collision, over the long haul,
+ * the error is near zero...
+ *
+ * @param op           [IN] pointer to specific protocol struct
+ * @param success      [IN] protocol error code == OK
+ * @param dup          [IN] true if op was detected duplicate
+ */
+
+static void record_op_only(struct proto_op *op, bool success, bool dup)
+{
+	/* count the op */
+	(void)atomic_inc_uint64_t(&op->total);
+	/* also count it as an error if protocol not happy */
+	if (!success)
+		(void)atomic_inc_uint64_t(&op->errors);
+	if (unlikely(dup))
+		(void)atomic_inc_uint64_t(&op->dups);
+}
 
 #ifdef USE_DBUS
 /**
@@ -910,7 +932,7 @@ static void record_layout(struct nfsv41_stats *sp, int proto_op, int status)
 static void record_nfsv4_op(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
 			    int proto_op, int minorversion,
 			    nsecs_elapsed_t request_time,
-			    int status)
+			    int status, bool is_export)
 {
 	if (minorversion == 0) {
 		struct nfsv40_stats *sp = get_v40(gsh_st, lock);
@@ -918,15 +940,27 @@ static void record_nfsv4_op(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
 		/* record stuff */
 		switch (nfsv40_optype[proto_op]) {
 		case READ_OP:
-			record_op(&sp->read.cmd, request_time,
+			if (is_export)
+				record_op(&sp->read.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->read.cmd,
 					status == NFS4_OK, false);
 			break;
 		case WRITE_OP:
-			record_op(&sp->write.cmd, request_time,
+			if (is_export)
+				record_op(&sp->write.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->write.cmd,
 					status == NFS4_OK, false);
 			break;
 		default:
-			record_op(&sp->compounds, request_time,
+			if (is_export)
+				record_op(&sp->compounds, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->compounds,
 					status == NFS4_OK, false);
 		}
 	} else if (minorversion == 1) {
@@ -935,19 +969,31 @@ static void record_nfsv4_op(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
 		/* record stuff */
 		switch (nfsv41_optype[proto_op]) {
 		case READ_OP:
-			record_op(&sp->read.cmd, request_time,
+			if (is_export)
+				record_op(&sp->read.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->read.cmd,
 					status == NFS4_OK, false);
 			break;
 		case WRITE_OP:
-			record_op(&sp->write.cmd, request_time,
+			if (is_export)
+				record_op(&sp->write.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->write.cmd,
 					status == NFS4_OK, false);
 			break;
 		case LAYOUT_OP:
 			record_layout(sp, proto_op, status);
 			break;
 		default:
-			record_op(&sp->compounds, request_time,
-				  status == NFS4_OK, false);
+			if (is_export)
+				record_op(&sp->compounds, request_time,
+					  status == NFS4_OK, false);
+			else
+				record_op_only(&sp->compounds,
+					  status == NFS4_OK, false);
 		}
 	} else if (minorversion == 2) {
 		struct nfsv41_stats *sp = get_v42(gsh_st, lock);
@@ -955,18 +1001,30 @@ static void record_nfsv4_op(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
 		/* record stuff */
 		switch (nfsv42_optype[proto_op]) {
 		case READ_OP:
-			record_op(&sp->read.cmd, request_time,
+			if (is_export)
+				record_op(&sp->read.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->read.cmd,
 					status == NFS4_OK, false);
 			break;
 		case WRITE_OP:
-			record_op(&sp->write.cmd, request_time,
+			if (is_export)
+				record_op(&sp->write.cmd, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->write.cmd,
 					status == NFS4_OK, false);
 			break;
 		case LAYOUT_OP:
 			record_layout(sp, proto_op, status);
 			break;
 		default:
-			record_op(&sp->compounds, request_time,
+			if (is_export)
+				record_op(&sp->compounds, request_time,
+					status == NFS4_OK, false);
+			else
+				record_op_only(&sp->compounds,
 					status == NFS4_OK, false);
 		}
 	}
@@ -1002,6 +1060,69 @@ static void record_compound(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
 	}
 
 }
+
+/**
+ * @brief Record request statistics (V3 era protos only)
+ *
+ * Decode the protocol and find the proto specific stats struct.
+ * Once we found the stats block, do the update(s).
+ *
+ * @param gsh_st       [IN] stats struct from client or export
+ * @param lock         [IN] lock on client|export for malloc
+ * @param reqdata      [IN] info about the proto request
+ * @param success      [IN] the op returned OK (or error)
+ * @param dup          [IN] detected this was a dup request
+ */
+
+static void record_clnt_stats(struct gsh_stats *gsh_st, pthread_rwlock_t *lock,
+			 request_data_t *reqdata, bool success, bool dup)
+{
+	struct svc_req *req = &reqdata->r_u.req.svc;
+	uint32_t proto_op = req->rq_msg.cb_proc;
+	uint32_t program_op = req->rq_msg.cb_prog;
+
+	if (program_op == NFS_program[P_NFS]) {
+		if (proto_op == 0)
+			return;	/* we don't count NULL ops */
+		if (req->rq_msg.cb_vers == NFS_V3) {
+			struct nfsv3_stats *sp = get_v3(gsh_st, lock);
+
+			/* record stuff */
+			switch (nfsv3_optype[proto_op]) {
+			case READ_OP:
+				record_op_only(&sp->read.cmd, success, dup);
+				break;
+			case WRITE_OP:
+				record_op_only(&sp->write.cmd, success, dup);
+				break;
+			default:
+				record_op_only(&sp->cmds, success, dup);
+			}
+		} else {
+			/* We don't do V4 here and V2 is toast */
+			return;
+		}
+	} else if (program_op == NFS_program[P_MNT]) {
+		struct mnt_stats *sp = get_mnt(gsh_st, lock);
+
+		if (req->rq_msg.cb_vers == MOUNT_V1)
+			record_op_only(&sp->v1_ops, success, dup);
+		else
+			record_op_only(&sp->v3_ops, success, dup);
+	} else if (program_op == NFS_program[P_NLM]) {
+		struct nlmv4_stats *sp = get_nlm4(gsh_st, lock);
+
+		record_op_only(&sp->ops, success, dup);
+	} else if (program_op == NFS_program[P_RQUOTA]) {
+		struct rquota_stats *sp = get_rquota(gsh_st, lock);
+
+		if (req->rq_msg.cb_vers == RQUOTAVERS)
+			record_op_only(&sp->ops, success, dup);
+		else
+			record_op_only(&sp->ext_ops, success, dup);
+	}
+}
+
 
 /**
  * @brief Record request statistics (V3 era protos only)
@@ -1217,9 +1338,8 @@ void server_stats_nfs_done(request_data_t *reqdata, int rc, bool dup)
 		struct server_stats *server_st;
 
 		server_st = container_of(client, struct server_stats, client);
-		record_stats(&server_st->st, &client->lock, reqdata,
-			     stop_time - op_ctx->start_time,
-			     rc == NFS_REQ_OK, dup, true);
+		record_clnt_stats(&server_st->st, &client->lock, reqdata,
+			     rc == NFS_REQ_OK, dup);
 		(void)atomic_store_uint64_t(&client->last_update, stop_time);
 	}
 	if (!dup && op_ctx->ctx_export != NULL) {
@@ -1230,7 +1350,7 @@ void server_stats_nfs_done(request_data_t *reqdata, int rc, bool dup)
 			    export);
 		record_stats(&exp_st->st, &op_ctx->ctx_export->lock, reqdata,
 			     stop_time - op_ctx->start_time,
-			     rc == NFS_REQ_OK, dup, false);
+			     rc == NFS_REQ_OK, dup, true);
 		(void)atomic_store_uint64_t(&op_ctx->ctx_export->last_update,
 					    stop_time);
 	}
@@ -1270,7 +1390,7 @@ void server_stats_nfsv4_op_done(int proto_op,
 		server_st = container_of(client, struct server_stats, client);
 		record_nfsv4_op(&server_st->st, &client->lock, proto_op,
 				op_ctx->nfs_minorvers, stop_time - start_time,
-				status);
+				status, false);
 		(void)atomic_store_uint64_t(&client->last_update, stop_time);
 	}
 
@@ -1292,7 +1412,7 @@ void server_stats_nfsv4_op_done(int proto_op,
 			    export);
 		record_nfsv4_op(&exp_st->st, &op_ctx->ctx_export->lock,
 				proto_op, op_ctx->nfs_minorvers,
-				stop_time - start_time, status);
+				stop_time - start_time, status, true);
 		(void)atomic_store_uint64_t(&op_ctx->ctx_export->last_update,
 					    stop_time);
 	}