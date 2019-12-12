ffilz/nfs-ganesha

Stats for all ops by a client
By default only IO related stats for a client are collected.
To get more precise workload information, stats for all operations
carried by a client are helpful.
Made a provision to gather and extract stats for all ops requested
by a client. This stat gathering is not enabled by default. One can
enable and disable such stat gather using ganesha_stats interface.

Change-Id: I9ec5c6a1066294186e2f4c731cff87b28a0c3a26
Signed-off-by: Sachin Punadikar <psachin@in.ibm.com>

src/support/client_mgr.c
@@ -255,6 +255,9 @@ int remove_gsh_client(sockaddr_t *client_ipaddr)
 	if (removed == 0) {
 		server_st = container_of(cl, struct server_stats, client);
 		server_stats_free(&server_st->st);
+		server_stats_allops_free(&server_st->c_all);
+		if (cl->hostaddr_str != NULL)
+			gsh_free(cl->hostaddr_str);
 		gsh_free(server_st);
 	}
 	return removed;
@@ -492,6 +495,27 @@ void reset_client_stats(void)
 					  node_k);
 		clnt = container_of(cl, struct server_stats, client);
 		reset_gsh_stats(&clnt->st);
+		/* reset stats counter for allops structs */
+		reset_gsh_allops_stats(&clnt->c_all);
+	}
+	PTHREAD_RWLOCK_unlock(&client_by_ip.lock);
+}
+
+/* Reset Client specific stats counters for allops
+ */
+void reset_clnt_allops_stats(void)
+{
+	struct avltree_node *client_node;
+	struct gsh_client *cl;
+	struct server_stats *clnt;
+
+	PTHREAD_RWLOCK_rdlock(&client_by_ip.lock);
+	for (client_node = avltree_first(&client_by_ip.t); client_node != NULL;
+	     client_node = avltree_next(client_node)) {
+		cl = avltree_container_of(client_node, struct gsh_client,
+					  node_k);
+		clnt = container_of(cl, struct server_stats, client);
+		reset_gsh_allops_stats(&clnt->c_all);
 	}
 	PTHREAD_RWLOCK_unlock(&client_by_ip.lock);
 }
@@ -533,7 +557,7 @@ static struct gsh_client *lookup_client(DBusMessageIter *args, char **errormsg)
 }
 
 /**
- * DBUS method to detailed client statistics
+ * DBUS method to get client IO ops statistics
  */
 static bool gsh_client_io_ops(DBusMessageIter *args,
 				  DBusMessage *reply,
@@ -568,6 +592,49 @@ static struct gsh_dbus_method cltmgr_client_io_ops = {
 		 STATUS_REPLY,
 		 TIMESTAMP_REPLY,
 		 CE_STATS_REPLY,
+		 END_ARG_LIST}
+};
+
+/**
+ * DBUS method to get all ops statistics for a client
+ */
+static bool gsh_client_all_ops(DBusMessageIter *args,
+				  DBusMessage *reply,
+				  DBusError *error)
+{
+	char *errormsg = "OK";
+	struct gsh_client *client = NULL;
+	bool success = true;
+	DBusMessageIter iter;
+
+	dbus_message_iter_init_append(reply, &iter);
+	if (!nfs_param.core_param.enable_CLNTALLSTATS) {
+		errormsg = "Stat counting for all ops for a client is disabled";
+		success = false;
+	} else {
+		client = lookup_client(args, &errormsg);
+		if (client == NULL) {
+			success = false;
+			errormsg = "Client IP address not found";
+		}
+	}
+
+	gsh_dbus_status_reply(&iter, success, errormsg);
+	if (success) {
+		server_dbus_client_all_ops(&iter, client);
+		put_gsh_client(client);
+	}
+
+	return true;
+}
+
+static struct gsh_dbus_method cltmgr_client_all_ops = {
+	.name = "GetClientAllops",
+	.method = gsh_client_all_ops,
+	.args = {IPADDR_ARG,
+		 STATUS_REPLY,
+		 TIMESTAMP_REPLY,
+		 CLNT_ALL_OPS_REPLY,
 		 END_ARG_LIST}
 };
 
@@ -969,6 +1036,7 @@ static struct gsh_dbus_method *cltmgr_stats_methods[] = {
 	&cltmgr_show_v41_layouts,
 	&cltmgr_show_delegations,
 	&cltmgr_client_io_ops,
+	&cltmgr_client_all_ops,
 #ifdef _USE_9P
 	&cltmgr_show_9p_io,
 	&cltmgr_show_9p_trans,

src/support/export_mgr.c
@@ -71,6 +71,7 @@ struct timespec fsal_stats_time;
 struct timespec v3_full_stats_time;
 struct timespec v4_full_stats_time;
 struct timespec auth_stats_time;
+struct timespec clnt_allops_stats_time;
 /**
  * @brief Exports are stored in an AVL tree with front-end cache.
  *
@@ -2149,7 +2150,7 @@ static bool stats_status(DBusMessageIter *args,
 {
 	bool success = true;
 	char *errormsg = "OK";
-	DBusMessageIter iter, nfsstatus, fsalstatus;
+	DBusMessageIter iter, nfsstatus, fsalstatus, clnt_allops_status;
 	DBusMessageIter v3_full_status, v4_full_status, authstatus;
 	dbus_bool_t value;
 
@@ -2199,6 +2200,15 @@ static bool stats_status(DBusMessageIter *args,
 	gsh_dbus_append_timestamp(&authstatus, &auth_stats_time);
 	dbus_message_iter_close_container(&iter, &authstatus);
 
+	/* Send info about client allops stats */
+	dbus_message_iter_open_container(&iter, DBUS_TYPE_STRUCT, NULL,
+						&clnt_allops_status);
+	value = nfs_param.core_param.enable_CLNTALLSTATS;
+	dbus_message_iter_append_basic(&clnt_allops_status, DBUS_TYPE_BOOLEAN,
+						&value);
+	gsh_dbus_append_timestamp(&clnt_allops_status, &clnt_allops_stats_time);
+	dbus_message_iter_close_container(&iter, &clnt_allops_status);
+
 	return true;
 }
 
@@ -2242,6 +2252,7 @@ static bool stats_disable(DBusMessageIter *args,
 		nfs_param.core_param.enable_FULLV3STATS = false;
 		nfs_param.core_param.enable_FULLV4STATS = false;
 		nfs_param.core_param.enable_AUTHSTATS = false;
+		nfs_param.core_param.enable_CLNTALLSTATS = false;
 		LogEvent(COMPONENT_CONFIG,
 			 "Disabling NFS server statistics counting");
 		LogEvent(COMPONENT_CONFIG,
@@ -2259,6 +2270,7 @@ static bool stats_disable(DBusMessageIter *args,
 		nfs_param.core_param.enable_NFSSTATS = false;
 		nfs_param.core_param.enable_FULLV3STATS = false;
 		nfs_param.core_param.enable_FULLV4STATS = false;
+		nfs_param.core_param.enable_CLNTALLSTATS = false;
 		LogEvent(COMPONENT_CONFIG,
 			 "Disabling NFS server statistics counting");
 		/* reset server stats counters */
@@ -2285,13 +2297,19 @@ static bool stats_disable(DBusMessageIter *args,
 		/* reset v4_full stats counters */
 		reset_v4_full_stats();
 	}
-
 	if (strcmp(stat_type, "auth") == 0) {
 		nfs_param.core_param.enable_AUTHSTATS = false;
 		LogEvent(COMPONENT_CONFIG,
 			"Disabling auth statistics counting");
 		/* reset auth counters */
 		reset_auth_stats();
+	}
+	if (strcmp(stat_type, "client_all_ops") == 0) {
+		nfs_param.core_param.enable_CLNTALLSTATS = false;
+		LogEvent(COMPONENT_CONFIG,
+			"Disabling client all ops statistics counting");
+		/* reset client all ops counters */
+		reset_clnt_allops_stats();
 	}
 
 	gsh_dbus_status_reply(&iter, true, errormsg);
@@ -2368,6 +2386,12 @@ static bool stats_enable(DBusMessageIter *args,
 					"Enabling auth statistics counting");
 			now(&auth_stats_time);
 		}
+		if (!nfs_param.core_param.enable_CLNTALLSTATS) {
+			nfs_param.core_param.enable_CLNTALLSTATS = true;
+			LogEvent(COMPONENT_CONFIG,
+				 "Enabling client all ops statistics counting");
+			now(&clnt_allops_stats_time);
+		}
 
 	}
 	if (strcmp(stat_type, "nfs") == 0 &&
@@ -2406,6 +2430,18 @@ static bool stats_enable(DBusMessageIter *args,
 			LogEvent(COMPONENT_CONFIG,
 			 "Enabling NFSv4 Detailed statistics counting");
 			now(&v4_full_stats_time);
+		}
+	}
+	if (strcmp(stat_type, "client_all_ops") == 0 &&
+			!nfs_param.core_param.enable_CLNTALLSTATS) {
+		if (!nfs_param.core_param.enable_NFSSTATS) {
+			errormsg = "First enable NFS stats counting";
+			goto error;
+		} else {
+			nfs_param.core_param.enable_CLNTALLSTATS = true;
+			LogEvent(COMPONENT_CONFIG,
+			 "Enabling client all ops statistics counting");
+			now(&clnt_allops_stats_time);
 		}
 	}
 

yogendra-t/nfs-ganesha-1

(cherry picked from commit a2b617b423792ffb4cffaa4d0066859c958e85c7)

src/support/client_mgr.c
@@ -334,6 +334,7 @@ int remove_gsh_client(sockaddr_t *client_ipaddr)
 	if (removed == 0) {
 		server_st = container_of(cl, struct server_stats, client);
 		server_stats_free(&server_st->st);
+		server_stats_allops_free(&server_st->c_all);
 		if (cl->hostaddr_str != NULL)
 			gsh_free(cl->hostaddr_str);
 		gsh_free(server_st);
@@ -574,6 +575,27 @@ void reset_client_stats(void)
 					  node_k);
 		clnt = container_of(cl, struct server_stats, client);
 		reset_gsh_stats(&clnt->st);
+		/* reset stats counter for allops structs */
+		reset_gsh_allops_stats(&clnt->c_all);
+	}
+	PTHREAD_RWLOCK_unlock(&client_by_ip.lock);
+}
+
+/* Reset Client specific stats counters for allops
+ */
+void reset_clnt_allops_stats(void)
+{
+	struct avltree_node *client_node;
+	struct gsh_client *cl;
+	struct server_stats *clnt;
+
+	PTHREAD_RWLOCK_rdlock(&client_by_ip.lock);
+	for (client_node = avltree_first(&client_by_ip.t); client_node != NULL;
+	     client_node = avltree_next(client_node)) {
+		cl = avltree_container_of(client_node, struct gsh_client,
+					  node_k);
+		clnt = container_of(cl, struct server_stats, client);
+		reset_gsh_allops_stats(&clnt->c_all);
 	}
 	PTHREAD_RWLOCK_unlock(&client_by_ip.lock);
 }
@@ -615,7 +637,7 @@ static struct gsh_client *lookup_client(DBusMessageIter *args, char **errormsg)
 }
 
 /**
- * DBUS method to detailed client statistics
+ * DBUS method to get client IO ops statistics
  */
 static bool gsh_client_io_ops(DBusMessageIter *args,
 				  DBusMessage *reply,
@@ -650,6 +672,49 @@ static struct gsh_dbus_method cltmgr_client_io_ops = {
 		 STATUS_REPLY,
 		 TIMESTAMP_REPLY,
 		 CE_STATS_REPLY,
+		 END_ARG_LIST}
+};
+
+/**
+ * DBUS method to get all ops statistics for a client
+ */
+static bool gsh_client_all_ops(DBusMessageIter *args,
+				  DBusMessage *reply,
+				  DBusError *error)
+{
+	char *errormsg = "OK";
+	struct gsh_client *client = NULL;
+	bool success = true;
+	DBusMessageIter iter;
+
+	dbus_message_iter_init_append(reply, &iter);
+	if (!nfs_param.core_param.enable_CLNTALLSTATS) {
+		errormsg = "Stat counting for all ops for a client is disabled";
+		success = false;
+	} else {
+		client = lookup_client(args, &errormsg);
+		if (client == NULL) {
+			success = false;
+			errormsg = "Client IP address not found";
+		}
+	}
+
+	dbus_status_reply(&iter, success, errormsg);
+	if (success) {
+		server_dbus_client_all_ops(&iter, client);
+		put_gsh_client(client);
+	}
+
+	return true;
+}
+
+static struct gsh_dbus_method cltmgr_client_all_ops = {
+	.name = "GetClientAllops",
+	.method = gsh_client_all_ops,
+	.args = {IPADDR_ARG,
+		 STATUS_REPLY,
+		 TIMESTAMP_REPLY,
+		 CLNT_ALL_OPS_REPLY,
 		 END_ARG_LIST}
 };
 
@@ -1051,6 +1116,7 @@ static struct gsh_dbus_method *cltmgr_stats_methods[] = {
 	&cltmgr_show_v41_layouts,
 	&cltmgr_show_delegations,
 	&cltmgr_client_io_ops,
+	&cltmgr_client_all_ops,
 #ifdef _USE_9P
 	&cltmgr_show_9p_io,
 	&cltmgr_show_9p_trans,

src/support/export_mgr.c
@@ -71,6 +71,7 @@ struct timespec fsal_stats_time;
 struct timespec v3_full_stats_time;
 struct timespec v4_full_stats_time;
 struct timespec auth_stats_time;
+struct timespec clnt_allops_stats_time;
 /**
  * @brief Exports are stored in an AVL tree with front-end cache.
  *
@@ -2151,7 +2152,7 @@ static bool stats_status(DBusMessageIter *args,
 {
 	bool success = true;
 	char *errormsg = "OK";
-	DBusMessageIter iter, nfsstatus, fsalstatus;
+	DBusMessageIter iter, nfsstatus, fsalstatus, clnt_allops_status;
 	DBusMessageIter v3_full_status, v4_full_status, authstatus;
 	dbus_bool_t value;
 
@@ -2201,6 +2202,15 @@ static bool stats_status(DBusMessageIter *args,
 	dbus_append_timestamp(&authstatus, &auth_stats_time);
 	dbus_message_iter_close_container(&iter, &authstatus);
 
+	/* Send info about client allops stats */
+	dbus_message_iter_open_container(&iter, DBUS_TYPE_STRUCT, NULL,
+						&clnt_allops_status);
+	value = nfs_param.core_param.enable_CLNTALLSTATS;
+	dbus_message_iter_append_basic(&clnt_allops_status, DBUS_TYPE_BOOLEAN,
+						&value);
+	dbus_append_timestamp(&clnt_allops_status, &clnt_allops_stats_time);
+	dbus_message_iter_close_container(&iter, &clnt_allops_status);
+
 	return true;
 }
 
@@ -2236,6 +2246,7 @@ static bool stats_disable(DBusMessageIter *args,
 		nfs_param.core_param.enable_FULLV3STATS = false;
 		nfs_param.core_param.enable_FULLV4STATS = false;
 		nfs_param.core_param.enable_AUTHSTATS = false;
+		nfs_param.core_param.enable_CLNTALLSTATS = false;
 		LogEvent(COMPONENT_CONFIG,
 			 "Disabling NFS server statistics counting");
 		LogEvent(COMPONENT_CONFIG,
@@ -2253,6 +2264,7 @@ static bool stats_disable(DBusMessageIter *args,
 		nfs_param.core_param.enable_NFSSTATS = false;
 		nfs_param.core_param.enable_FULLV3STATS = false;
 		nfs_param.core_param.enable_FULLV4STATS = false;
+		nfs_param.core_param.enable_CLNTALLSTATS = false;
 		LogEvent(COMPONENT_CONFIG,
 			 "Disabling NFS server statistics counting");
 		/* reset server stats counters */
@@ -2279,14 +2291,19 @@ static bool stats_disable(DBusMessageIter *args,
 		/* reset v4_full stats counters */
 		reset_v4_full_stats();
 	}
-
-	dbus_status_reply(&iter, success, errormsg);
 	if (strcmp(stat_type, "auth") == 0) {
 		nfs_param.core_param.enable_AUTHSTATS = false;
 		LogEvent(COMPONENT_CONFIG,
 			"Disabling auth statistics counting");
 		/* reset auth counters */
 		reset_auth_stats();
+	}
+	if (strcmp(stat_type, "client_all_ops") == 0) {
+		nfs_param.core_param.enable_CLNTALLSTATS = false;
+		LogEvent(COMPONENT_CONFIG,
+			"Disabling client all ops statistics counting");
+		/* reset client all ops counters */
+		reset_clnt_allops_stats();
 	}
 
 	dbus_status_reply(&iter, success, errormsg);
@@ -2352,6 +2369,12 @@ static bool stats_enable(DBusMessageIter *args,
 					"Enabling auth statistics counting");
 			now(&auth_stats_time);
 		}
+		if (!nfs_param.core_param.enable_CLNTALLSTATS) {
+			nfs_param.core_param.enable_CLNTALLSTATS = true;
+			LogEvent(COMPONENT_CONFIG,
+				 "Enabling client all ops statistics counting");
+			now(&clnt_allops_stats_time);
+		}
 
 	}
 	if (strcmp(stat_type, "nfs") == 0 &&
@@ -2390,6 +2413,18 @@ static bool stats_enable(DBusMessageIter *args,
 			LogEvent(COMPONENT_CONFIG,
 			 "Enabling NFSv4 Detailed statistics counting");
 			now(&v4_full_stats_time);
+		}
+	}
+	if (strcmp(stat_type, "client_all_ops") == 0 &&
+			!nfs_param.core_param.enable_CLNTALLSTATS) {
+		if (!nfs_param.core_param.enable_NFSSTATS) {
+			errormsg = "First enable NFS stats counting";
+			success = false;
+		} else {
+			nfs_param.core_param.enable_CLNTALLSTATS = true;
+			LogEvent(COMPONENT_CONFIG,
+			 "Enabling client all ops statistics counting");
+			now(&clnt_allops_stats_time);
 		}
 	}
 