madhuthorat/nfs-ganesha

[GPFS] Fix to let GPFS upcall thread exit at ganesha startup
Currently during startup if GPFSFSAL_UP_Thread() gets called, it waits
in nfs_init_wait() for init to complete. But if during startup
gpfs_unclaim_filesystem() gets called, then the main thread may hang
at pthread_join() for GPFS upcall thread if the thread is waiting in
nfs_init_wait().
To avoid this made use of 'stop_thread' which is set to 'true' prior to
calling pthread_join() in gpfs_unclaim_filesystem(). Also made changes
to allow to pass timeout value to nfs_init_wait() called in
GPFSFSAL_UP_Thread(), and after nfs_init_wait() completes we check
if stop_thread==true then we return.

Signed-off-by: Madhu Thorat <madhu.punjabi@in.ibm.com>
Co-authored-by: Malahal Naineni <malahal@us.ibm.com>

src/include/nfs_init.h
@@ -54,7 +54,7 @@ pthread_t gsh_dbus_thrid;
 
 void nfs_init_init(void);
 void nfs_init_complete(void);
-void nfs_init_wait(void);
+int nfs_init_wait(int timeout);
 
 /**
  * nfs_prereq_init:

yogendra-t/nfs-ganesha-1

Currently during startup if GPFSFSAL_UP_Thread() gets called, it
waits in nfs_init_wait() for init to complete. But if during startup
at pthread_join() for GPFS upcall thread, if the thread is waiting in
To avoid this made use of 'stop_thread' variable in
'struct gpfs_filesystem', which is set to 'true' prior to calling
pthread_join() in gpfs_unclaim_filesystem(). Also have added
nfs_init_wait_timeout(int timeout), which is now called in
GPFSFSAL_UP_Thread() in while(1) loop, and after nfs_init_wait_timeout()
completes if gpfs_fs->stop_thread==true then we return.
Change-Id: I8cdf193ce955cd7248104741927307974d8f0958
Reviewed-by: Malahal Naineni <malahal@us.ibm.com>
(cherry picked from commit 0e4fb74e32c2361e444e4a88ad2b9f175bef614d)
Conflicts:
	src/MainNFSD/libganesha_nfsd.ver
(cherry picked from commit 39dfd79879f9d4afcd0307a00e9b78f138cefc1e)

src/include/nfs_init.h
@@ -55,6 +55,7 @@ pthread_t gsh_dbus_thrid;
 void nfs_init_init(void);
 void nfs_init_complete(void);
 void nfs_init_wait(void);
+int nfs_init_wait_timeout(int timeout);
 
 /**
  * nfs_prereq_init: